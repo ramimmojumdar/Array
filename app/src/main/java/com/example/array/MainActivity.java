2fe53d71095f529503c92445c3d31990d97d3196
ramimmojumdar <expertramim@gmial.com>
2024-05-19 16:56:41 +0600

    arry add

--- app/src/main/java/com/example/array/MainActivity.java
+++ app/src/main/java/com/example/array/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.array;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +12,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView arrayText;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arrayText = findViewById(R.id.arrayText);
 
         String[] name = {"Ramim", "Shamim", "Naim"};
+
+        for(int x=0; x<name.length; x++){
+
+            arrayText.append(name[x] + "\n");
+
+        }
+
+
+
 
     }
 }
